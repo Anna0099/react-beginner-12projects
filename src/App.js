b3b63055136527a0b73beef6a22614b309a73eb9
Anna0099 <anna4taide@gmail.com>
2024-03-20 17:45:32 +0300

    2 modals

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import './index.scss';
+import Modal1 from "./Modal1";
+import Modal2 from "./Modal2";
 
 function App() {
+    const  [open1,setOpen1] = React.useState(false);
+    const  [open2,setOpen2] = React.useState(false);
   return (
     <div className="App">
-      <button className="open-modal-btn">✨ Открыть окно</button>
-      {/* <div className="overlay">
-        <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200">
-            <title />
-            <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
-          </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        </div>
-      </div> */}
+      <button onClick={ ()=>setOpen1(true)} className="open-modal-btn">
+          ✨ Открыть окно условного рендера
+      </button>
+      <button onClick={ ()=>setOpen2(true)} className="open-modal-btn">
+          ✨ Открыть окно анимированого рендера
+      </button>
+        {open1 &&
+        <Modal1 open={open1} setOpen={setOpen1}>
+            <h2>Модалка условного рендера</h2>
+        </Modal1>}
+        <Modal2 open={open2} setOpen={setOpen2}/>
     </div>
   );
 }
